2703d104cb2ec30940f20e0ddc43815d6440c0b3
Adam Stelmaszyk <adamstelm98@gmail.com>
2020-05-03 15:18:38 +0200

    Connect with api the application and created first view version

---
+++ src/const/index.js
@@ -0,0 +1,2 @@
+export const WEB = "https://api.themoviedb.org/3/search/movie?api_key=2fedf296580966b93eb49959b902a4a9&"
+export const IMAGE_WEB = "https://image.tmdb.org/t/p/w220_and_h330_face"
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,19 +1,70 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+// import { Button, ActivityIndicator, FlatList, Text, View } from 'react-native';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+    Container,
+    Row,
+    Header, Title, List, ListItem, Thumbnail, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Item, Input
+} from 'native-base';
+import { WEB, IMAGE_WEB } from "../const"
+
 
 function HomeScreen({ navigation }) {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(WEB + 'language=en-US&query=a&page=1&include_adult=false')
+            .then((response) => response.json())
+            .then((json) => setData(json.results))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    const list = (data.map(element => (
+        <ListItem thumbnail>
+            <Left>
+                <Thumbnail square source={{ uri: IMAGE_WEB + element.poster_path }} />
+            </Left>
+            <Body>
+                <Text>{element.title}</Text>
+                <Row style={{ alignItems: "center" }}>
+                    <Icon active name="star" />
+                    <Text note numberOfLines={1}> {element.popularity}</Text>
+                </Row>
+                <Row style={{ alignItems: "center" }}>
+                    <Text note numberOfLines={1}>Vote: </Text>
+                    <Text note numberOfLines={1}>{element.vote_count}</Text>
+                </Row>
+            </Body>
+            <Right>
+                <Button transparent>
+                    <Text>View</Text>
+                </Button>
+            </Right>
+        </ListItem>
+    )))
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => navigation.navigate('Details',
-                    {
-                        itemId: 106,
-                        otherParam: 'anything you want here',
-                    })}
-            />
-        </View>
+        <Container>
+            <Content>
+                <Item>
+                    <Input placeholder='Find your movie' />
+                    <Icon active name='search' />
+                </Item>
+                <List>
+                    {list}
+                </List>
+            </Content>
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text>Footer</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>
+
     );
 }
 
